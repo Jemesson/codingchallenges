cc79a030d16686420de1e566003e5291be8be0a3
Jemesson <jemessonlima@gmail.com>
2018-12-13 22:55:28 -0300

    add doc

--- problem1.py
+++ problem1.py
@@ -2,9 +2,16 @@ import random
 import timeit
 
 
-def sum_equals_num(numbers: list, x: int) -> bool:
+def sum_equals_num(numbers: list, k: int) -> bool:
+    """
+    This problem was recently asked by Google.
+    Given a list of numbers and a number k, return whether any two numbers from the list add up to k
+    :param numbers:
+    :param k:
+    :return: Boolean two numbers added matches k
+    """
     for i in range(0, len(numbers)):
-        res = x - numbers[i]
+        res = k - numbers[i]
         if res >= 0 and res in numbers:
             return True
 
@@ -12,15 +19,15 @@ def sum_equals_num(numbers: list, x: int) -> bool:
 
 
 if __name__ == '__main__':
-    total_nums = 1000000
-    k = 517
+    total_nums = 1000
+    goal_number = 517
 
     nums = []
     for i in range(0, total_nums):
         nums.append(random.randint(1, total_nums))
 
     start_time = timeit.default_timer()
-    result = sum_equals_num(nums, k)
+    result = sum_equals_num(nums, goal_number)
     end_time = timeit.default_timer() - start_time
 
     print(f'{result}. Executed in {end_time}s.')
